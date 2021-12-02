654b1d29fbe076d86a0fa53311924a4132208ba6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-12-02 12:48:31 -0500

    Added BasketBall, BowlingBall and BaseBall

--- Assets/[Scripts]/GameController.cs
+++ Assets/[Scripts]/GameController.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Sphere Launch Properties")]
     public List<GameObject> spherePrefabs;
     public float frameDelay;
     public float Offset;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetAxis("Fire1") > 0) && (Time.frameCount % frameDelay == 0))
+        if ((Input.GetAxisRaw("Fire1") > 0) && (Time.frameCount % frameDelay == 0))
         {
             var randomSphereIndex = Random.Range(0, spherePrefabs.Count);
             var bullet = Instantiate(spherePrefabs[randomSphereIndex], Camera.main.transform.position + Camera.main.transform.forward * Offset, Quaternion.identity);
--- Assets/[Scripts]/SphereBehaviour.cs
+++ Assets/[Scripts]/SphereBehaviour.cs
@@ -8,12 +8,6 @@ public class SphereBehaviour : MonoBehaviour
     public float speed;
     public Vector3 direction;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
